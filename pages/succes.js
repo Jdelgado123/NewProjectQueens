b1a8fcb8ce7b5a004044af3de187da17407951e2
unknown <jean.delgado@tecsup.edu.pe>
2022-06-10 14:16:34 -0500

    generador de qr

--- pages/succes.js
+++ pages/succes.js
@@ -5,7 +5,7 @@ import { BsBagCheckFill } from 'react-icons/bs'
 import { useRouter } from 'next/router'
 import Layout from '../components/Layout'
 import {runFireworks} from '../utils/confetti'
-
+import { QRCodeSVG } from 'qrcode.react'
 
 const succes = () => {
 
@@ -17,11 +17,12 @@ const succes = () => {
     return (
         <Layout>
         <div className='success-wrapper mx-auto min-h-screen place-content-center grid w-full'>
-            <div className='bg-gray-400/100 mx-auto items-center place-content-center w-full flex justify-center flex-col p-12 rounded-lg gap-6'>
+            <div className='bg-gray-400/100 mx-auto items-center place-content-center w-full flex justify-center flex-col px-80 rounded-lg gap-6'>
                 <p className='icon'>
                     <BsBagCheckFill></BsBagCheckFill>
                 </p>
                 <h2>PEDIDO</h2>
+                <div><QRCodeSVG value='001425558'></QRCodeSVG></div>
                 <h1 className='text-red-500/100 text-4x1'>0014255</h1>
                 <h2 className='description'>PASAR POR CAJA</h2  >
             </div>
