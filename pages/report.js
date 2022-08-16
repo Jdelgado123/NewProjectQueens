e84337512473996d3a031272a1fb8b13e59a6017
unknown <jean.delgado@tecsup.edu.pe>
2022-08-16 16:40:53 -0500

    Agregado de modal de prestamo en caja

--- pages/report.js
+++ pages/report.js
@@ -21,7 +21,7 @@ function report() {
                     <div className="flex items-center justify-between px-8 py-2 border border-solid cursor-pointer rounded-xl dark:border-gray-700" onClick={()=> getReport(30)}>
                         <div className="flex flex-col items-center space-y-1">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-400 sm:h-7 sm:w-7" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                             </svg>
 
                             <h2 className="text-lg font-medium text-gray-700 sm:text-xl dark:text-gray-200">Seleccionar</h2>
@@ -33,7 +33,7 @@ function report() {
                     <div className="flex items-center justify-between px-8 py-2 border border-solid border-blue-500 cursor-pointer rounded-xl" onClick={()=> getReport(7)}>
                         <div className="flex flex-col items-center space-y-1">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 dark:text-blue-500 sm:h-7 sm:w-7" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                             </svg>
 
                             <h2 className="text-lg font-medium text-gray-700 sm:text-xl dark:text-gray-200">Seleccionar</h2>
@@ -48,7 +48,7 @@ function report() {
                     <div className="flex items-center justify-between px-8 py-2 border border-solid cursor-pointer rounded-xl dark:border-gray-700" onClick={()=> getReport(1)}>
                         <div className="flex flex-col items-center space-y-1">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-gray-400 sm:h-7 sm:w-7" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                             </svg>
 
                             <h2 className="text-lg font-medium text-gray-700 sm:text-xl dark:text-gray-200">Seleccionar</h2>
