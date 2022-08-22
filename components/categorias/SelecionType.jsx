7362809af559852f83f49308b8b46e84ba6e3b05
unknown <jean.delgado@tecsup.edu.pe>
2022-08-22 16:35:49 -0500

    csmr

--- components/categorias/SelecionType.jsx
+++ components/categorias/SelecionType.jsx
@@ -5,17 +5,10 @@ const SelecionType = () => {
   return (
     <div className='grid gap-1 md:gap-6 grid-cols-1 md:grid-cols-1 mx-auto container'>
                 <Link href={`/categorias/mujer`} >
-<<<<<<< HEAD
-                    <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Lady`s</button>
-                </Link>
-                <Link href={`/categorias/hombre`} >
-                    <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Daddy`s</button>
-=======
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Ladies</button>
                 </Link>
                 <Link href={`/categorias/hombre`} >
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Daddys</button>
->>>>>>> 1ee18efeec5cfbb1b6a5fdfed9ee428577ce4ff8
                 </Link>
                 <Link href={`/categories/peluches`} >
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Toys</button>
@@ -23,13 +16,9 @@ const SelecionType = () => {
                 <Link href={`/categorias/sexShop`} >
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">SexShop</button>
                 </Link>
-<<<<<<< HEAD
+
                 <Link href={`/categories/ofertas`} >
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Offers</button>
-=======
-                <Link href={`/ofertas`} >
-                    <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Ofertas</button>
->>>>>>> 1ee18efeec5cfbb1b6a5fdfed9ee428577ce4ff8
                 </Link>
                 <Link href={`/categorias/unisex`} >
                     <button className="text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg hover:bg-indigo-800 h-12 px-6 m-2 text-lg">Home</button>
